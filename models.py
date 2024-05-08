44b130e94c0d81e4a3a573a6b0706ba9f7d53ee8
DavidePaglieri <paglieridavide@gmail.com>
2024-05-08 18:57:46 +0100

    feat: Cohere support

--- models.py
+++ models.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 
 
-
 def find_sublayers(module, layers=None, name=""):
     if not layers:
         layers = [transformers.pytorch_utils.Conv1D, nn.Conv2d, nn.Linear]
@@ -12,7 +11,11 @@ def find_sublayers(module, layers=None, name=""):
             return {name: module}
     res = {}
     for name1, child in module.named_children():
-        res.update(find_sublayers(child, layers=layers, name=name + "." + name1 if name != "" else name1))
+        res.update(
+            find_sublayers(
+                child, layers=layers, name=name + "." + name1 if name != "" else name1
+            )
+        )
     return res
 
 
@@ -20,7 +23,7 @@ def get_layers(model, module_name: str):
     for name, module in model.named_modules():
         if name.startswith(module_name):
             return module
-        
+
 
 def get_model_type(model_name):
     if model_name in CAUSAL_LM_MODEL_MAP:
@@ -29,7 +32,7 @@ def get_model_type(model_name):
         raise ValueError(f"Model {model_name} not found in CAUSAL_LM_MODEL_MAP")
 
 
-class GPT2CausalLM():
+class GPT2CausalLM:
     layer_type = "GPT2Block"
     layers_block_name = "transformer.h"
     outside_layer_modules = ["transformer.wte", "transformer.wpe", "transformer.ln_f"]
@@ -41,7 +44,7 @@ class GPT2CausalLM():
     ]
 
 
-class MistralCausalLM():
+class MistralCausalLM:
     layer_type = "MistralDecoderLayer"
     layers_block_name = "model.layers"
     outside_layer_modules = ["model.embed_tokens", "model.norm"]
@@ -53,7 +56,7 @@ class MistralCausalLM():
     ]
 
 
-class GemmaCausalLM():
+class GemmaCausalLM:
     layer_type = "GemmaDecoderLayer"
     layers_block_name = "model.layers"
     outside_layer_modules = ["model.embed_tokens", "model.norm"]
@@ -65,8 +68,20 @@ class GemmaCausalLM():
     ]
 
 
-class LlamaCausalLM():
+class LlamaCausalLM:
     layer_type = "LlamaDecoderLayer"
+    layers_block_name = "model.layers"
+    outside_layer_modules = ["model.embed_tokens", "model.norm"]
+    inside_layer_modules = [
+        ["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"],
+        ["self_attn.o_proj"],
+        ["mlp.up_proj", "mlp.gate_proj"],
+        ["mlp.down_proj"],
+    ]
+
+
+class CohereCausalLM:
+    layer_type = "CohereDecoderLayer"
     layers_block_name = "model.layers"
     outside_layer_modules = ["model.embed_tokens", "model.norm"]
     inside_layer_modules = [
@@ -82,4 +97,13 @@ CAUSAL_LM_MODEL_MAP = {
     "llama": LlamaCausalLM,
     "mistral": MistralCausalLM,
     "gemma": GemmaCausalLM,
+}
+
+
+CAUSAL_LM_MODEL_MAP = {
+    "gpt2": GPT2CausalLM,
+    "llama": LlamaCausalLM,
+    "mistral": MistralCausalLM,
+    "gemma": GemmaCausalLM,
+    "cohere": CohereCausalLM,
 }
